3c8ec432375921d8299506a486405214853358c0
Rattle_Brain <danialgo2001@gmail.com>
2024-02-07 11:49:04 +0000

    TCP server funcionando

--- Sesion2-Procesos/Sesion2_1/prodcons.c
+++ Sesion2-Procesos/Sesion2_1/prodcons.c
@@ -4,35 +4,36 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void main ()
+void main()
 {
     int fds[2];
-    pid_t pid;
+    pid_t pid_h1, pid_h2;
+    FILE f;
 
-    if(pipe(fds) < 0)
-    {   // Comprobar que se crea el pipe correctamente
-        perror("Fallo al crear el pipe\n");
+    if (pipe(fds) < 0)
+    {
+        perror("Fallo al crear el pipe");
         exit(EXIT_FAILURE);
     }
 
-    if((pid = fork()) < 0)
+    if((pid_h1 = fork()) < 0)
     {   // Fallo al crear el primer fork
         perror("Fallo al crear el primer fork");
         exit(EXIT_FAILURE + 1);
     }
-    else if(pid == 0)
+    else if(pid_h1 == 0)
     {   //Retorno del fork en el 1er hijo
         dup2(fds[0], 0);
         close(fds[1]);
         execlp("sort", "sort", NULL);
     }
     else{
-        if((pid = fork()) < 0)
+        if((pid_h2 = fork()) < 0)
         {   // Fallo al crear el segundo fork
             perror("Fallo al crear el segundo fork");
             exit(EXIT_FAILURE + 1);
         }
-        else if(pid == 0)
+        else if(pid_h2 == 0)
         {   // Retorno del fork en el segundo hijo
             dup2(fds[1], 1);
             close(fds[0]);
---
+++ Sesion2-Procesos/Sesion2_2/servidor-echo-tcp-basico.c
@@ -0,0 +1,48 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MSG_LEN 100
+
+int main(int argc, char* argv[]) {
+
+    int sock_pasivo, sock_datos;
+    struct sockaddr_in d_local;
+    char mensaje[MSG_LEN];
+    int leidos;
+
+    if(argc < 2)
+    {
+        perror("Es necesario incluir al menos un puerto paara la conexión\n");
+        perror("\tservidor-echo-tcp-basico [port]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+    if(port < 1024 || port > 65536)
+    {
+        perror("El puerto está fuera de rango válido\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sock_pasivo = socket(PF_INET, SOCK_STREAM, 0);      // Comprobar valor devuelto por socket
+    d_local.sin_family = AF_INET;
+    d_local.sin_addr.s_addr = htonl(INADDR_ANY);
+    d_local.sin_port = htons(port);
+
+    bind(sock_pasivo, (struct sockaddr *)&d_local, sizeof(d_local)); // Comprobar valor devuelto por bind
+    listen(sock_pasivo, SOMAXCONN);     // Comprobar valor devuelto por listen
+
+    while (1) { // Bucle infinito de atención a clientes
+        sock_datos = accept(sock_pasivo, 0, 0);
+        while((leidos = read(sock_datos, mensaje, sizeof(mensaje))) > 0)
+        {
+            send(sock_datos, mensaje, leidos, 0);
+        }
+        send(sock_datos, mensaje, strlen(mensaje), 0);
+        close(sock_datos);
+    }
+}
