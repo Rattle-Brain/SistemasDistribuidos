24b1f03f426ac7f4a3282acfafd673411530e2a9
Rattle-Brain <danialgo2001@gmail.com>
2024-03-17 20:44:06 +0100

    Decodificar texto funciona

--- Sesion3_ReprInfo/Sesion3_2-XDR/lee-datos-texto.c
+++ Sesion3_ReprInfo/Sesion3_2-XDR/lee-datos-texto.c
@@ -4,12 +4,11 @@
 #include "tipos.h"
 #include <rpc/xdr.h>
 
-#define MAX_TEXTO 100
-
-int main() {
+int main()
+{
     // Abrir el archivo Texto.dat para lectura binaria
-    FILE *archivo = fopen("Texto.dat", "rb");
-    if (archivo == NULL) {
+    FILE *file_text = fopen("Texto.dat", "rb");
+    if (file_text == NULL) {
         perror("Error al abrir el archivo Texto.dat");
         return 1;
     }
@@ -18,18 +17,18 @@ int main() {
     Texto texto;
     
     // Crear un flujo XDR para leer desde el archivo
-    XDR xdrs;
-    xdrstdio_create(&xdrs, archivo, XDR_DECODE);
+    XDR xdr_text;
+    xdrstdio_create(&xdr_text, file_text, XDR_DECODE);
     
     // Leer los datos del archivo y almacenarlos en la variable de tipo Texto utilizando el filtro xdr_Texto()
-    if (!xdr_Texto(&xdrs, &texto)) {
+    if (!xdr_Texto(&xdr_text, &texto)) {
         fprintf(stderr, "Error al leer los datos del archivo Texto.dat\n");
         return 1;
     }
     
     // Destruir el flujo XDR y cerrar el archivo
-    xdr_destroy(&xdrs);
-    fclose(archivo);
+    xdr_destroy(&xdr_text);
+    fclose(file_text);
     
     // Mostrar el texto decodificado
     printf("Texto decodificado: %s\n", texto);
