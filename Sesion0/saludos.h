77ca6a5d756a65c58755429ab3029107f6b4e871
Rattle-Brain <danialgo2001@gmail.com>
2024-03-17 18:44:06 +0100

    Fixed warning

--- Sesion0/saludos.h
+++ Sesion0/saludos.h
@@ -1,9 +1,8 @@
+#include <stdio.h>
+
 #define NUMSALUDOS 10
 
 
 
 // Appends \n to normal string so you don't have to.
-void printl(char* string)
-{
-     printf("%s\n", string);
-}
+inline void printl(char* string) { printf("%s\n", string); }
