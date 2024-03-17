8342efeae91ace7ae7bd31bbe3a1590ffac7682d
Rattle-Brain <danialgo2001@gmail.com>
2024-03-17 19:48:58 +0100

    Volcado Resultado.dat XDR funciona

--- Sesion3_ReprInfo/Sesion3_2-XDR/genera-datos.c
+++ Sesion3_ReprInfo/Sesion3_2-XDR/genera-datos.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <rpc/xdr.h>
 #include <stdlib.h>
+#include <string.h>
 #include "tipos.h"
+
+#define MAX_TEXTO 100
 
 int main() {
 
@@ -50,8 +53,6 @@ int main() {
 
     persona.nombre = "Maria";
     persona.edad = 22;
-
-    perror("Variables asignadas correctamente -Persona\n");
     
     // Abre el archivo Persona.dat para escritura binaria
     FILE *file_person = fopen("Persona.dat", "wb");
@@ -59,21 +60,16 @@ int main() {
         perror("Error al abrir el archivo Persona.dat");
         exit(EXIT_FAILURE);
     }
-
-    perror("Fichero abierto correctamente\n");
     
     // Crea un flujo XDR para escribir en el archivo
     XDR xdr_person;
     xdrstdio_create(&xdr_person, file_person, XDR_ENCODE);
-    perror("Filtro XDR_person creado correctamente\n");
     
     // Volca la variable de tipo Persona al archivo utilizando el filtro xdr_Persona()
     if (!xdr_Persona(&xdr_person, &persona)) {
         fprintf(stderr, "Error al volcar la variable de tipo Persona al archivo\n");
         exit(EXIT_FAILURE);
     }
-
-    perror("Llamada a xdr_Persona() terminada correctamente\n");
     
     // Destruye el flujo XDR y cierra el archivo
     xdr_destroy(&xdr_person);
@@ -81,5 +77,75 @@ int main() {
     
     printf("Se ha volcado la variable de tipo Persona al archivo Persona.dat correctamente.\n");
     
+    // PARTE 3 - RESULTADO
+
+    // Presentar el menú al usuario
+    printf("Elige una opcion:\n");
+    printf("    1. Entero\n");
+    printf("    2. Real\n");
+    printf("    3. Texto\n");
+    printf("Tu eleccion: ");
+    
+    // Leer la elección del usuario
+    int opcion;
+    scanf("%d", &opcion);
+    
+    // Validar la elección del usuario
+    if (opcion < 1 || opcion > 3) {
+        fprintf(stderr, "Opcion invalida. Debe ser 1, 2 o 3.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Asignar la elección del usuario al discriminante de la variable de tipo Resultado
+    resultado.caso = opcion;
+    
+    // Pedir al usuario el dato correspondiente según su elección
+    switch (opcion) {
+        case 1:
+            printf("Introduce un numero entero: ");
+            scanf("%d", &resultado.Resultado_u.n);
+            break;
+        case 2:
+            printf("Introduce un numero real: ");
+            scanf("%f", &resultado.Resultado_u.x);
+            break;
+        case 3:
+            printf("Introduce un texto: ");
+            // Hay que hacer un Mallo antes de nada o SIGSEGV
+            resultado.Resultado_u.error = malloc(sizeof(char) * MAX_TEXTO);
+            scanf("%s", resultado.Resultado_u.error);
+            break;
+    }
+
+    perror("Variable asignada a campo Resultado\n");
+    
+    // Abre el archivo Resultado.dat para escritura binaria
+    FILE *file_result = fopen("Resultado.dat", "wb");
+    if (file_result == NULL) {
+        perror("Error al abrir el archivo Resultado.dat");
+        exit(EXIT_FAILURE);
+    }
+    perror("Resultado.dat creado\n");
+    
+    // Crea un flujo XDR para escribir en el archivo
+    XDR xdr_result;
+    xdrstdio_create(&xdr_result, file_result, XDR_ENCODE);
+
+    perror("XDR creado\n");
+    
+    // Volca la variable de tipo Resultado al archivo utilizando el filtro xdr_Resultado()
+    if (!xdr_Resultado(&xdr_result, &resultado)) {
+        fprintf(stderr, "Error al volcar la variable de tipo Resultado al archivo\n");
+        exit(EXIT_FAILURE);
+    }
+
+    perror("xdr_Resultado ejecutada correctamente\n");
+    
+    // Destruye el flujo XDR y cierra el archivo
+    xdr_destroy(&xdr_result);
+    fclose(file_result);
+    
+    printf("Se ha volcado la variable de tipo Resultado al archivo Resultado.dat correctamente.\n");
+
     exit(EXIT_SUCCESS);
 }
