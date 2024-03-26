fb35de72036c907fc502f92e1ed939a1589b76cb
Rattle-Brain <danialgo2001@gmail.com>
2024-03-26 18:42:46 +0100

    inicializador terminado

--- Entregable/PrimeraParte/p2/inicializador_srvdns.c
+++ Entregable/PrimeraParte/p2/inicializador_srvdns.c
@@ -44,11 +44,12 @@ int main(int argc, char *argv[])
 
     // Inicializar la estructura CLIENT para las llamadas a procedimientos remotos
     // A RELLENAR
-    |
-    |
-    |
-    |
-    |
+    cl = clnt_create(ip_srvdns, SRVDNS, PRIMERA, "tcp");
+
+    if (cl == NULL) {
+        fprintf(stderr, "Problema inicializando la estructura CLIENTE\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Inicializar la estructura dat con las listas de dominios y tipos de registros
     dat.nomdominios = obtener_lista_dominios(argv[2]);
@@ -143,9 +144,8 @@ Lista *obtener_lista_tiposregistros(char *fichrecords)
 
         // Si ya estaba en la lista, saltamos esta iteración
         // A RELLENAR
-        |
-        |
-        |
+        if (posicion_en_lista(token, inicio) != NOENCONTRADO)
+            continue;
 
 
         // En caso contrario, debe crearse un nuevo elemento para este nombre
