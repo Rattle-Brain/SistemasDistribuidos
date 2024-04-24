53f5eea374eae052967d82748da662a6b4efdcbc
Rattle-Brain <danialgo2001@gmail.com>
2024-04-24 19:17:49 +0200

    Reformatted Filosofos

--- PracticasLab/Sesion6-Coordinacion/Sesion6_1-Centralizada/filosofos.c
+++ PracticasLab/Sesion6-Coordinacion/Sesion6_1-Centralizada/filosofos.c
@@ -6,42 +6,7 @@ En esta version distribuida del problema de los filosofos requiere de un
 maestro que controle los recursos compartidos (palillos).
 
 *****************************************************************************/
-#include <stdio.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-// Funciones para simplificar la API de sockets. Se declaran aquí
-// pero no se implementan. El alumno puede copiar su implementación
-// de otros ejercicios, o implementarlas él mismo
-
-// Crea un socket y si no hay errores lo retorna
-int CrearSocketClienteTCP(void);
-
-// Conecta a la IP y puertos dados. La IP se le pasa como cadena
-// Si hay errores termina el programa
-void ConectarConServidor(int s, char *ip, int puerto);
-
-// Las dos siguientes funciones equivalen a send() y recv() pero
-// si detectan un error terminan el programa
-int Enviar(int s, char *buff, int longitud);
-int Recibir(int s, char *buff, int longitud);
-
-// Cierra el socket
-void CerrarSocket(int s);
-
-#define MAX_BOCADOS 10
-#define LOCK 1
-#define UNLOCK 2
-
-//Prototipos de las funciones
-void filosofo(int numfilo,char *ip,int puerto,int maxfilo);
-int controlMutex(char *ipmaster, int puerto, char op, int nummutex);
+#include "filosofos.h"
 
 int main(int argc, char *argv[])
 {
@@ -141,7 +106,7 @@ int controlMutex(char *ipmaster, int puerto, char op, int nummutex)
             printf(buffer,"U %d",nummutex);
             break;
     }
-    
+
     //se envía el mensaje al maestro de recursos
     Enviar(sock,buffer,strlen(buffer));
     //se inicializa el buffer para la recepción
