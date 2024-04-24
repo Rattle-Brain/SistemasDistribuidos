6f55c45f4f5ff958f6ec1be1140fe8c4f4dcd26a
Rattle-Brain <danialgo2001@gmail.com>
2024-04-24 19:16:34 +0200

    Reformatted maestro

--- PracticasLab/Sesion6-Coordinacion/Sesion6_1-Centralizada/maestro.c
+++ PracticasLab/Sesion6-Coordinacion/Sesion6_1-Centralizada/maestro.c
@@ -1,41 +1,5 @@
-// Implementación de un maestro de recursos para
-// coordinacion distribuida
-#include <sys/socket.h> // socket, send, recv...
-#include <netinet/in.h> // sockaddr_in, htons, htonl, etc.
-#include <arpa/inet.h>  // inet_addr
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <pthread.h>  //prototipos de funciones para mutex
 
-#define MAX_LINEA 80 // Maxima longitud esperada para una lectura
-
-//Estructura que se le va a pasar a cada hilo que atiende una petición
-typedef struct {
-    int puerto;
-    pthread_mutex_t * lista;
-    int tamlista;
-}
-petinfo;
-
-// Esta función crea el socket de escucha, le asigna el número de
-// puerto y le pone en modo "listen". Retorna el socket creado.
-// El código de la función está más abajo (después del main())
-int InicializarSocketEscucha(int puerto);
-
-// Esta función hace una lectura del socket de datos.  Si recibe cero,
-// retorna. En otro caso, lee del socket, interpreta el mensaje,
-// hace la operacion correspondiente sobre la posicion del array de mutex
-// y envia confirmacion
-void OperarMutex(petinfo * pet, int socket_datos, int * lista, int listalen);
-
-// Esta funcion inicializa cada posicion del array de mutex
-
-void InicializaMutex(pthread_mutex_t * lista, int tam);
-
-//Procesa una nueva peticion de acceso a un recurso
-void * procesaPeticion(petinfo * p);
+#include "maestro.h"
 
 // Programa principal.
 // Crea un socket de escucha y queda a la espera de un cliente.
