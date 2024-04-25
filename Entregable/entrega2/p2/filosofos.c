4974a79233e18175626978676de48979c11867ec
Rattle-Brain <danialgo2001@gmail.com>
2024-04-25 20:29:13 +0200

    modified full directory

--- Entregable/entrega2/p2/filosofos.c
+++ Entregable/entrega2/p2/filosofos.c
@@ -6,107 +6,141 @@ En esta version distribuida del problema de los filosofos requiere de un
 maestro que controle los recursos compartidos (palillos).
 
 *****************************************************************************/
-#include "filosofos.h"
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Funciones para simplificar la API de sockets. Se declaran aquí
+// pero no se implementan. El alumno puede copiar su implementación
+// de otros ejercicios, o implementarlas él mismo
+
+// Crea un socket y si no hay errores lo retorna
+int CrearSocketClienteTCP(void);
+
+// Conecta a la IP y puertos dados. La IP se le pasa como cadena
+// Si hay errores termina el programa
+void ConectarConServidor(int s, char *ip, int puerto);
+
+// Las dos siguientes funciones equivalen a send() y recv() pero
+// si detectan un error terminan el programa
+int Enviar(int s, char *buff, int longitud);
+int Recibir(int s, char *buff, int longitud);
+
+// Cierra el socket
+void CerrarSocket(int s);
+
+#define MAX_BOCADOS 10
+#define LOCK 1
+#define UNLOCK 2
+
+//Prototipos de las funciones
+void filosofo(int numfilo,char *ip,int puerto,int maxfilo);
+int controlMutex(char *ipmaster, int puerto, char op, int nummutex);
 
 int main(int argc, char *argv[])
 {
-    int numfilo;
-    int puerto;
-    int maxfilo;
+   int numfilo;
+   int puerto;
+   int maxfilo;
 
-    //Comprobacion del número de argumentos
-    if (argc<5)
-    {
-        fprintf(stderr,"Forma de uso: %s numfilo ip_maestro puerto maxfilo\n",
-                argv[0]);
-        exit(1);
-    }
-    /*** Lectura de valores de los argumentos ***/
-    numfilo=atoi(argv[1]);
-    if ((numfilo<0) || (numfilo>4))
-    {
-        fprintf(stderr,"El numero de filosofo debe ser >=0 y <=4\n");
-        exit(2);
-    }
-    puerto=atoi(argv[3]);
-    if ((puerto<0) || (puerto>65535))
-    {
-        fprintf(stderr,"El numero de puerto debe ser >=0 y <=65535\n");
-        exit(3);
-    }
-    maxfilo=atoi(argv[4]);
-    if (maxfilo<0)
-    {
-        fprintf(stderr,"El numero de filosofos debe ser mayor que 0\n");
-        exit(3);
-    }
-    /*********************************************/
-    //lanzamiento del filósofo
-    filosofo(numfilo,argv[2],puerto,maxfilo);
-    exit(0);
+   //Comprobacion del número de argumentos
+   if (argc<5)
+   {
+      fprintf(stderr,"Forma de uso: %s numfilo ip_maestro puerto maxfilo\n",
+              argv[0]);
+    exit(1);
+   }
+   /*** Lectura de valores de los argumentos ***/
+   numfilo=atoi(argv[1]);
+   if ((numfilo<0) || (numfilo>4))
+   {
+      fprintf(stderr,"El numero de filosofo debe ser >=0 y <=4\n");
+    exit(2);
+   }
+   puerto=atoi(argv[3]);
+   if ((puerto<0) || (puerto>65535))
+   {
+      fprintf(stderr,"El numero de puerto debe ser >=0 y <=65535\n");
+    exit(3);
+   }
+   maxfilo=atoi(argv[4]);
+   if (maxfilo<0)
+   {
+      fprintf(stderr,"El numero de filosofos debe ser mayor que 0\n");
+    exit(3);
+   }
+   /*********************************************/
+   //lanzamiento del filósofo
+   filosofo(numfilo,argv[2],puerto,maxfilo);
+   exit(0);
 }
 
 //función que implementa las tareas a realizar por el filósofo
 void filosofo(int numfilo,char *ip,int puerto,int maxfilo)
 {
-    int veces=0;
+  int veces=0;
 
-    printf("Filosofo %d sentado a la mesa.\n",numfilo);
-    while (veces<MAX_BOCADOS)
-    {
-        // mientras el acuse de la solicitud de palillo derecho sea 0
-        // seguimos intentándolo
-        while (controlMutex(ip,puerto,LOCK,numfilo)==0);
-        // mientras el acuse de la solicitud de palillo izquierdo sea 0
-        // seguimos intentándolo
-        while (controlMutex(ip,puerto,LOCK,(numfilo+1)%maxfilo)==0);
-        // ya tenemos ambos palillos y por tanto podemos comer
-        printf("El filosofo %d esta comiendo.\n",numfilo);
-        sleep(3);
-        // mientras el acuse de liberación de palillo izquierdo sea 0
-        // seguimos intentando
-        //liberar el palillo
-        while (controlMutex(ip,puerto,UNLOCK,(numfilo+1)%maxfilo)==0);
-        // mientras el acuse de liberación de palillo derecho sea 0
-        // seguimos intentando
-        // liberar el palillo
-        while (controlMutex(ip,puerto,UNLOCK,numfilo)==0);
-        //el filosofo ha soltado ambos palillos y puede dedicarse a pensar
-        printf("El filosofo %d esta pensando.\n",numfilo);
-        sleep(5);
-        // incrementamos el número de veces que el filósofo
-        // ha podido completar el ciclo
-        veces++;
-    }
-    //el filósofo ha completado el número de bocados y se levanta de la mesa
-    printf("El filosofo %d se ha levantado de la mesa.\n",numfilo);
+  printf("Filosofo %d sentado a la mesa.\n",numfilo);
+  while (veces<MAX_BOCADOS)
+  {
+       // mientras el acuse de la solicitud de palillo derecho sea 0
+       // seguimos intentándolo
+       while (controlMutex(ip,puerto,LOCK,numfilo)==0);
+       // mientras el acuse de la solicitud de palillo izquierdo sea 0
+       // seguimos intentándolo
+       while (controlMutex(ip,puerto,LOCK,(numfilo+1)%maxfilo)==0);
+       // ya tenemos ambos palillos y por tanto podemos comer
+       printf("El filosofo %d esta comiendo.\n",numfilo);
+       sleep(3);
+       // mientras el acuse de liberación de palillo izquierdo sea 0
+       // seguimos intentando
+       //liberar el palillo
+       while (controlMutex(ip,puerto,UNLOCK,(numfilo+1)%maxfilo)==0);
+       // mientras el acuse de liberación de palillo derecho sea 0
+       // seguimos intentando
+       // liberar el palillo
+       while (controlMutex(ip,puerto,UNLOCK,numfilo)==0);
+       //el filosofo ha soltado ambos palillos y puede dedicarse a pensar
+       printf("El filosofo %d esta pensando.\n",numfilo);
+       sleep(5);
+       // incrementamos el número de veces que el filósofo
+       // ha podido completar el ciclo
+       veces++;
+  }
+  //el filósofo ha completado el número de bocados y se levanta de la mesa
+  printf("El filosofo %d se ha levantado de la mesa.\n",numfilo);
 }
 
 //esta función le permite al filósofo solicitar-liberar un recurso
 //gestionado por el coordinador
 int controlMutex(char *ipmaster, int puerto, char op, int nummutex)
 {
-    //Necesita de las funciones de libsokets
-    int sock;
-    char buffer[10];
-    char *ptr;
+   //Necesita de las funciones de libsokets
+   int sock;
+   char buffer[10];
+   char *ptr;
 
-    //crea el socket de conexion con el maestro de recursos
-    sock=CrearSocketClienteTCP();
-    //abre conexión con el maestro de recursos
-    ConectarConServidor(sock, ipmaster, puerto);
-    //en buffer confecciona el mensaje en función de la operación
-    //a realizar y el palillo a solicitar (LOCK) o liberar (UNLOCK)
-    sprintf(buffer,"%c",'\0');
-    switch (op){
-        case LOCK:
-            sprintf(buffer,"L %d",nummutex);
-            break;
-        case UNLOCK:
-            printf(buffer,"U %d",nummutex);
-            break;
+   //crea el socket de conexion con el maestro de recursos
+   sock=CrearSocketClienteTCP();
+   //abre conexión con el maestro de recursos
+   ConectarConServidor(sock, ipmaster, puerto);
+   //en buffer confecciona el mensaje en función de la operación
+   //a realizar y el palillo a solicitar (LOCK) o liberar (UNLOCK)
+   sprintf(buffer,"%c",'\0');
+   switch (op){
+     case LOCK:
+     sprintf(buffer,"L %d",nummutex);
+     break;
+     case UNLOCK:
+     sprintf(buffer,"U %d",nummutex);
+     break;
     }
-
     //se envía el mensaje al maestro de recursos
     Enviar(sock,buffer,strlen(buffer));
     //se inicializa el buffer para la recepción
@@ -122,15 +156,10 @@ int controlMutex(char *ipmaster, int puerto, char op, int nummutex)
     return(atoi(ptr));
 }
 
-/*
-###################################################################
-###################################################################
-###################################################################
-###################################################################
-*/
-
+// FASE 1:
 // Funciones que faltaban
 
+// FASE 1:
 // Devuelve el fd del socket TCP creado
 int CrearSocketClienteTCP(void){
     int socketTCP;
@@ -144,9 +173,10 @@ int CrearSocketClienteTCP(void){
     return socketTCP;
 }
 
+// FASE 1:
 // Con los parametros que recibe trata de conectarse a un servidor
 // Si falla pues para la ejecucion.
-void ConectarConServidor(int sockfd, char *ip, int puerto) {
+void ConectarConServidor(int s, char *ip, int puerto) {
     struct sockaddr_in servaddr;
 
     // Configurar la dirección del servidor
@@ -155,16 +185,17 @@ void ConectarConServidor(int sockfd, char *ip, int puerto) {
     servaddr.sin_port = htons(puerto); // Convertir el puerto de host a orden de red
 
     // Conectar al servidor
-    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
+    if (connect(s, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
         perror("Error al conectar con el servidor");
-        close(sockfd);
+        close(s);
         exit(EXIT_FAILURE);
     }
 }
 
+// FASE 1:
 // Función para cerrar un socket
-void CerrarSocket(int sockfd) {
-    int resultado = close(sockfd);
+void CerrarSocket(int s) {
+    int resultado = close(s);
     if (resultado == -1) {
         perror("Error al cerrar el socket");
         exit(EXIT_FAILURE);
@@ -183,6 +214,7 @@ int Enviar(int s, char *buff, int longitud) {
     return rec_bytes;
 }
 
+// FASE 1:
 // Función para recibir datos a través de un socket
 // Si hay errores, termina el programa
 int Recibir(int s, char *buff, int longitud) {
