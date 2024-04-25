6fa37193c23058f26fdd416a2f15432e2c1f937e
Rattle-Brain <danialgo2001@gmail.com>
2024-04-25 20:40:31 +0200

    Fase 2 completa

--- Entregable/entrega2/p2/filosofos.c
+++ Entregable/entrega2/p2/filosofos.c
@@ -6,7 +6,8 @@ En esta version distribuida del problema de los filosofos requiere de un
 maestro que controle los recursos compartidos (palillos).
 
 *****************************************************************************/
-#include <stdio.h>
+#include <stdio.h>      // FASE 2
+#include <time.h>       // FASE 2
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,108 +40,134 @@ void CerrarSocket(int s);
 #define LOCK 1
 #define UNLOCK 2
 
+#define LOG_MSG_SIZE 50     // FASE 2: Define tamaño de la variable log_msg.
+
 //Prototipos de las funciones
 void filosofo(int numfilo,char *ip,int puerto,int maxfilo);
 int controlMutex(char *ipmaster, int puerto, char op, int nummutex);
 
+// FASE 2:
+void log_debug(char* msg); 
+
+char * log_msg = NULL;      // FASE 2: Declaracion de la variable necesaria
+
 int main(int argc, char *argv[])
 {
-   int numfilo;
-   int puerto;
-   int maxfilo;
+    int numfilo;
+    int puerto;
+    int maxfilo;
 
-   //Comprobacion del número de argumentos
-   if (argc<5)
-   {
-      fprintf(stderr,"Forma de uso: %s numfilo ip_maestro puerto maxfilo\n",
-              argv[0]);
-    exit(1);
-   }
-   /*** Lectura de valores de los argumentos ***/
-   numfilo=atoi(argv[1]);
-   if ((numfilo<0) || (numfilo>4))
-   {
-      fprintf(stderr,"El numero de filosofo debe ser >=0 y <=4\n");
-    exit(2);
-   }
-   puerto=atoi(argv[3]);
-   if ((puerto<0) || (puerto>65535))
-   {
-      fprintf(stderr,"El numero de puerto debe ser >=0 y <=65535\n");
-    exit(3);
-   }
-   maxfilo=atoi(argv[4]);
-   if (maxfilo<0)
-   {
-      fprintf(stderr,"El numero de filosofos debe ser mayor que 0\n");
-    exit(3);
-   }
-   /*********************************************/
-   //lanzamiento del filósofo
-   filosofo(numfilo,argv[2],puerto,maxfilo);
-   exit(0);
+    setbuf(stdout, NULL);        // FASE 2: Cancelacion de la buferizacion
+    log_msg = malloc(sizeof(char) * LOG_MSG_SIZE);
+
+    //Comprobacion del número de argumentos
+    if (argc<5)
+    {
+        fprintf(stderr,"Forma de uso: %s numfilo ip_maestro puerto maxfilo\n",
+                argv[0]);
+        exit(1);
+    }
+    /*** Lectura de valores de los argumentos ***/
+    numfilo=atoi(argv[1]);
+    if ((numfilo<0) || (numfilo>4))
+    {
+        fprintf(stderr,"El numero de filosofo debe ser >=0 y <=4\n");
+        exit(2);
+    }
+    puerto=atoi(argv[3]);
+    if ((puerto<0) || (puerto>65535))
+    {
+        fprintf(stderr,"El numero de puerto debe ser >=0 y <=65535\n");
+        exit(3);
+    }
+    maxfilo=atoi(argv[4]);
+    if (maxfilo<0)
+    {
+        fprintf(stderr,"El numero de filosofos debe ser mayor que 0\n");
+        exit(3);
+    }
+    /*********************************************/
+    //lanzamiento del filósofo
+    filosofo(numfilo,argv[2],puerto,maxfilo);
+    free (log_msg);     // FASE 2: Free del malloc
+    exit(0);
 }
 
 //función que implementa las tareas a realizar por el filósofo
 void filosofo(int numfilo,char *ip,int puerto,int maxfilo)
 {
-  int veces=0;
+    int veces=0;
 
-  printf("Filosofo %d sentado a la mesa.\n",numfilo);
-  while (veces<MAX_BOCADOS)
-  {
-       // mientras el acuse de la solicitud de palillo derecho sea 0
-       // seguimos intentándolo
-       while (controlMutex(ip,puerto,LOCK,numfilo)==0);
-       // mientras el acuse de la solicitud de palillo izquierdo sea 0
-       // seguimos intentándolo
-       while (controlMutex(ip,puerto,LOCK,(numfilo+1)%maxfilo)==0);
-       // ya tenemos ambos palillos y por tanto podemos comer
-       printf("El filosofo %d esta comiendo.\n",numfilo);
-       sleep(3);
-       // mientras el acuse de liberación de palillo izquierdo sea 0
-       // seguimos intentando
-       //liberar el palillo
-       while (controlMutex(ip,puerto,UNLOCK,(numfilo+1)%maxfilo)==0);
-       // mientras el acuse de liberación de palillo derecho sea 0
-       // seguimos intentando
-       // liberar el palillo
-       while (controlMutex(ip,puerto,UNLOCK,numfilo)==0);
-       //el filosofo ha soltado ambos palillos y puede dedicarse a pensar
-       printf("El filosofo %d esta pensando.\n",numfilo);
-       sleep(5);
-       // incrementamos el número de veces que el filósofo
-       // ha podido completar el ciclo
-       veces++;
-  }
-  //el filósofo ha completado el número de bocados y se levanta de la mesa
-  printf("El filosofo %d se ha levantado de la mesa.\n",numfilo);
+    // FASE 2: log_debug
+    sprintf(log_msg, "Filosofo %d sentado a la mesa.\n", numfilo);
+    log_debug(log_msg);
+    while (veces<MAX_BOCADOS)
+    {
+        // mientras el acuse de la solicitud de palillo derecho sea 0
+        // seguimos intentándolo
+        while (controlMutex(ip,puerto,LOCK,numfilo)==0);
+        // mientras el acuse de la solicitud de palillo izquierdo sea 0
+        // seguimos intentándolo
+        while (controlMutex(ip,puerto,LOCK,(numfilo+1)%maxfilo)==0);
+        // ya tenemos ambos palillos y por tanto podemos comer
+
+        // FASE 2: log_debug
+        sprintf(log_msg, "El filosofo %d esta comiendo.\n", numfilo);
+        log_debug(log_msg);
+
+        sleep(3);
+        // mientras el acuse de liberación de palillo izquierdo sea 0
+        // seguimos intentando
+        //liberar el palillo
+        while (controlMutex(ip,puerto,UNLOCK,(numfilo+1)%maxfilo)==0);
+        // mientras el acuse de liberación de palillo derecho sea 0
+        // seguimos intentando
+        // liberar el palillo
+        while (controlMutex(ip,puerto,UNLOCK,numfilo)==0);
+        //el filosofo ha soltado ambos palillos y puede dedicarse a pensar
+
+        // FASE 2: log_debug
+        sprintf(log_msg, "El filosofo %d esta pensando.\n", numfilo);
+        log_debug(log_msg);
+
+        sleep(5);
+        // incrementamos el número de veces que el filósofo
+        // ha podido completar el ciclo
+        veces++;
+    }
+    //el filósofo ha completado el número de bocados y se levanta de la mesa
+
+    // FASE 2: log_debug
+    sprintf(log_msg, "El filosofo %d se ha levantado de la mesa.\n", numfilo);
+    log_debug(log_msg);
 }
 
 //esta función le permite al filósofo solicitar-liberar un recurso
 //gestionado por el coordinador
 int controlMutex(char *ipmaster, int puerto, char op, int nummutex)
 {
-   //Necesita de las funciones de libsokets
-   int sock;
-   char buffer[10];
-   char *ptr;
+    //Necesita de las funciones de libsokets
+    int sock;
+    char buffer[10];
+    char *ptr;
 
-   //crea el socket de conexion con el maestro de recursos
-   sock=CrearSocketClienteTCP();
-   //abre conexión con el maestro de recursos
-   ConectarConServidor(sock, ipmaster, puerto);
-   //en buffer confecciona el mensaje en función de la operación
-   //a realizar y el palillo a solicitar (LOCK) o liberar (UNLOCK)
-   sprintf(buffer,"%c",'\0');
-   switch (op){
-     case LOCK:
-     sprintf(buffer,"L %d",nummutex);
-     break;
-     case UNLOCK:
-     sprintf(buffer,"U %d",nummutex);
-     break;
+    //crea el socket de conexion con el maestro de recursos
+    sock=CrearSocketClienteTCP();
+    //abre conexión con el maestro de recursos
+    ConectarConServidor(sock, ipmaster, puerto);
+    //en buffer confecciona el mensaje en función de la operación
+    //a realizar y el palillo a solicitar (LOCK) o liberar (UNLOCK)
+    sprintf(buffer,"%c",'\0');
+
+    switch (op){
+        case LOCK:
+            sprintf(buffer,"L %d",nummutex);
+            break;
+        case UNLOCK:
+            sprintf(buffer,"U %d",nummutex);
+            break;
     }
+
     //se envía el mensaje al maestro de recursos
     Enviar(sock,buffer,strlen(buffer));
     //se inicializa el buffer para la recepción
@@ -225,4 +252,13 @@ int Recibir(int s, char *buff, int longitud) {
         exit(EXIT_FAILURE);
     }
     return rec_bytes;
+}
+
+
+void log_debug(char *msg)
+{
+    struct timespec t;
+    clock_gettime(_POSIX_MONOTONIC_CLOCK, &t);
+
+    printf("[%ld.%09ld] %s", t.tv_sec, t.tv_nsec, msg);
 }
